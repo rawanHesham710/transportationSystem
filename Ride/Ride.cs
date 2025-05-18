b74689bf4eebda8a1beb37c5cc353ec996af1b65
Mariamelsawyy <159478931+Mariamelsawyy@users.noreply.github.com>
2025-05-18 19:22:08 +0300

    Update Ride.cs

--- Ride/Ride.cs
+++ Ride/Ride.cs
@@ -1,138 +1,192 @@
-using System;
-using Microsoft.Data.SqlClient;
-using System.Windows.Forms;
-using System.Configuration;
-
 namespace WindowsFormsApp1
 {
-    public partial class Ride : Form
+    partial class Ride
     {
-        public Ride()
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
         {
-            InitializeComponent();
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
         {
-            try
-            {
-                string connectionString = ConfigurationManager.ConnectionStrings["DatabaseConnectionString"].ConnectionString;
+            this.txtuserId = new System.Windows.Forms.TextBox();
+            this.txtdestination = new System.Windows.Forms.TextBox();
+            this.txtstartAddress = new System.Windows.Forms.TextBox();
+            this.txtstartTime = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtPromoCode = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtuserId
+            // 
+            this.txtuserId.BackColor = System.Drawing.SystemColors.Window;
+            this.txtuserId.Location = new System.Drawing.Point(12, 43);
+            this.txtuserId.Name = "txtuserId";
+            this.txtuserId.Size = new System.Drawing.Size(379, 20);
+            this.txtuserId.TabIndex = 0;
+            // 
+            // txtdestination
+            // 
+            this.txtdestination.AccessibleName = "";
+            this.txtdestination.Location = new System.Drawing.Point(12, 107);
+            this.txtdestination.Name = "txtdestination";
+            this.txtdestination.Size = new System.Drawing.Size(379, 20);
+            this.txtdestination.TabIndex = 2;
+            // 
+            // txtstartAddress
+            // 
+            this.txtstartAddress.Location = new System.Drawing.Point(12, 168);
+            this.txtstartAddress.Name = "txtstartAddress";
+            this.txtstartAddress.Size = new System.Drawing.Size(379, 20);
+            this.txtstartAddress.TabIndex = 3;
+            // 
+            // txtstartTime
+            // 
+            this.txtstartTime.Location = new System.Drawing.Point(12, 237);
+            this.txtstartTime.Name = "txtstartTime";
+            this.txtstartTime.Size = new System.Drawing.Size(379, 20);
+            this.txtstartTime.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "userId\r\n\r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(9, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 18);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "startAddress";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(9, 216);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 18);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "startTime";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(9, 86);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 18);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "destination";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.button1.Location = new System.Drawing.Point(593, 353);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 51);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "CONFIRM";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(9, 283);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(105, 18);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Promo-Code";
+            // 
+            // txtPromoCode
+            // 
+            this.txtPromoCode.Location = new System.Drawing.Point(12, 304);
+            this.txtPromoCode.Name = "txtPromoCode";
+            this.txtPromoCode.Size = new System.Drawing.Size(379, 20);
+            this.txtPromoCode.TabIndex = 12;
+            // 
+            // Ride
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtPromoCode);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtstartTime);
+            this.Controls.Add(this.txtstartAddress);
+            this.Controls.Add(this.txtdestination);
+            this.Controls.Add(this.txtuserId);
+            this.Name = "Ride";
+            this.Text = "Ride";
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
-                TimeSpan tripDuration = CalculateTripDuration(txtstartAddress.Text, txtdestination.Text);
-                TimeSpan startTime = TimeSpan.Parse(txtstartTime.Text);
-                TimeSpan endTime = startTime.Add(tripDuration);
-
-                int randomDriverId = GetRandomDriverId(connectionString);
-
-                string sqlQuery = @"INSERT INTO ride 
-            (destination, startAddress, startTime, endTime, feedback, score, userId, driverId) 
-            VALUES 
-            (@Destination, @StartAddress, @StartTime, @EndTime,@Feedback,@Score, @UserId, @DriverId);
-            SELECT SCOPE_IDENTITY();";
-
-                using (SqlConnection con = new SqlConnection(connectionString))
-                {
-                    con.Open();
-                    using (SqlCommand cmd = new SqlCommand(sqlQuery, con))
-                    {
-                        var destinationParameter = new SqlParameter("@Destination", System.Data.SqlDbType.VarChar);
-                        destinationParameter.Value = txtdestination.Text;
-                        cmd.Parameters.Add(destinationParameter);
-
-                        var startAddressParameter = new SqlParameter("@StartAddress", System.Data.SqlDbType.VarChar);
-                        startAddressParameter.Value = txtstartAddress.Text;
-                        cmd.Parameters.Add(startAddressParameter);
-
-                        var startTimeParameter = new SqlParameter("@StartTime", System.Data.SqlDbType.Time);
-                        startTimeParameter.Value = startTime;
-                        cmd.Parameters.Add(startTimeParameter);
-
-                        var endTimeParameter = new SqlParameter("@EndTime", System.Data.SqlDbType.Time);
-                        endTimeParameter.Value = endTime;
-                        cmd.Parameters.Add(endTimeParameter);
-
-                        var userIdParameter = new SqlParameter("@UserId", System.Data.SqlDbType.Int);
-                        userIdParameter.Value = int.Parse(txtuserId.Text);
-                        cmd.Parameters.Add(userIdParameter);
-
-                        var feedbackParameter = new SqlParameter("@Feedback", System.Data.SqlDbType.VarChar);
-                        feedbackParameter.Value = " ";
-                        cmd.Parameters.Add(feedbackParameter);
-
-                        var scoreParameter = new SqlParameter("@Score", System.Data.SqlDbType.VarChar);
-                        scoreParameter.Value = " ";
-                        cmd.Parameters.Add(scoreParameter);
-
-                        var driverIdParameter = new SqlParameter("@DriverId", System.Data.SqlDbType.Int);
-                        driverIdParameter.Value = randomDriverId;
-                        cmd.Parameters.Add(driverIdParameter);
-
-                        object result = cmd.ExecuteScalar();
-                        if (result == null || result == DBNull.Value)
-                        {
-                            MessageBox.Show("Failed to get ride ID.");
-                            return;
-                        }
-                        int rideId = Convert.ToInt32(result);
-                        MessageBox.Show($"Ride created successfully! Estimated arrival: {endTime.ToString(@"hh\:mm")} Ride ID: {rideId}");
-
-                        FeedbackForm feedbackForm = new FeedbackForm(rideId);
-                        feedbackForm.ShowDialog();
-                    }
-                }
-            }
-            catch (FormatException)
-            {
-                MessageBox.Show("Please enter time in correct format (HH:mm)");
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Error: {ex.Message}");
-            }
         }
 
-        private int GetRandomDriverId(string connectionString)
-        {
-            try
-            {
-                using (SqlConnection con = new SqlConnection(connectionString))
-                {
-                    con.Open();
-                    string query = "SELECT TOP 1 id FROM driver ORDER BY NEWID()";
-                    using (SqlCommand cmd = new SqlCommand(query, con))
-                    {
-                        object result = cmd.ExecuteScalar();
-                        if (result != null)
-                        {
-                            return Convert.ToInt32(result);
-                        }
-                    }
-                }
-            }
-            catch (Exception)
-            {
-                // Ignore database errors and fall through to random number generation
-            }
+        #endregion
 
-            Random random = new Random();
-            return random.Next(1, 1001);
-        }
-
-        private TimeSpan CalculateTripDuration(string startAddress, string destination)
-        {
-            double distanceKm = EstimateDistance(startAddress, destination);
-            double durationHours = distanceKm / 40.0;
-            return TimeSpan.FromMinutes(Math.Max(durationHours * 60, 15));
-        }
-
-        private double EstimateDistance(string start, string end)
-        {
-            if (start.Contains("downtown") && end.Contains("airport"))
-                return 25.0;
-            else if (start.Contains("main") && end.Contains("center"))
-                return 8.0;
-            else
-                return 15.0;
-        }
+        private System.Windows.Forms.TextBox txtuserId;
+        private System.Windows.Forms.TextBox txtdestination;
+        private System.Windows.Forms.TextBox txtstartAddress;
+        private System.Windows.Forms.TextBox txtstartTime;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtPromoCode;
     }
 }
+
