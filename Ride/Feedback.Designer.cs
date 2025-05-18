6d1c389b37faea855fd6240fde47b6168dd4323c
Mariamelsawyy <159478931+Mariamelsawyy@users.noreply.github.com>
2025-05-18 16:15:33 +0300

    Add files via upload

---
+++ Ride/Feedback.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class FeedbackForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.submit = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCommentComboBox = new System.Windows.Forms.ComboBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // submit
+            // 
+            this.submit.BackColor = System.Drawing.SystemColors.Highlight;
+            this.submit.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.submit.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.submit.Location = new System.Drawing.Point(259, 384);
+            this.submit.Name = "submit";
+            this.submit.Size = new System.Drawing.Size(191, 50);
+            this.submit.TabIndex = 15;
+            this.submit.Text = "SUBMIT";
+            this.submit.UseVisualStyleBackColor = false;
+            this.submit.Click += new System.EventHandler(this.submit_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 161);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 24);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Score";
+            // 
+            // txtCommentComboBox
+            // 
+            this.txtCommentComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCommentComboBox.FormattingEnabled = true;
+            this.txtCommentComboBox.Items.AddRange(new object[] {
+            "5",
+            "4",
+            "3",
+            "2",
+            "1"});
+            this.txtCommentComboBox.Location = new System.Drawing.Point(16, 188);
+            this.txtCommentComboBox.Name = "txtCommentComboBox";
+            this.txtCommentComboBox.Size = new System.Drawing.Size(181, 26);
+            this.txtCommentComboBox.TabIndex = 14;
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(12, 280);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(328, 24);
+            this.txtName.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 253);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 24);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Your Feedback ";
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::WindowsFormsApp1.Properties.Resources.star_empty;
+            this.pictureBox5.Location = new System.Drawing.Point(415, 71);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(33, 52);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 6;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.star_empty;
+            this.pictureBox1.Location = new System.Drawing.Point(259, 71);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(33, 52);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::WindowsFormsApp1.Properties.Resources.star_empty;
+            this.pictureBox2.Location = new System.Drawing.Point(376, 71);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(33, 52);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::WindowsFormsApp1.Properties.Resources.star_empty;
+            this.pictureBox3.Location = new System.Drawing.Point(337, 71);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(33, 52);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::WindowsFormsApp1.Properties.Resources.star_empty;
+            this.pictureBox4.Location = new System.Drawing.Point(298, 71);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(33, 52);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 5;
+            this.pictureBox4.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(272, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(168, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Your Rating";
+            // 
+            // FeedbackForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 27F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(722, 461);
+            this.Controls.Add(this.submit);
+            this.Controls.Add(this.txtCommentComboBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Font = new System.Drawing.Font("NSimSun", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(8, 6, 8, 6);
+            this.Name = "FeedbackForm";
+            this.Text = "Feedback";
+            this.Load += new System.EventHandler(this.FeedbackForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button submit;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox txtCommentComboBox;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ Ride/Feedback.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace WindowsFormsApp1
+{
+    public partial class FeedbackForm : Form
+    {
+        private int rideId;
+
+        public FeedbackForm(int rideId)
+        {
+            InitializeComponent();
+            this.rideId = rideId;
+            if (txtCommentComboBox.Items.Count == 0)
+            {
+                txtCommentComboBox.Items.AddRange(new object[] { 1, 2, 3, 4, 5 });
+            }
+            MessageBox.Show($"FeedbackForm opened with Ride ID: {rideId}");
+        }
+
+        //private void btnSave_Click(object sender, EventArgs e)
+        //{
+        //}
+
+        private void FeedbackForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void submit_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                string feedback = txtName.Text.Trim();
+                if (string.IsNullOrEmpty(feedback))
+                {
+                    MessageBox.Show("Feedback is empty. Please enter feedback.");
+                    return;
+                }
+
+                if (txtCommentComboBox.SelectedItem == null)
+                {
+                    MessageBox.Show("Please select a score.");
+                    return;
+                }
+                int score;
+                if (!int.TryParse(txtCommentComboBox.SelectedItem.ToString(), out score))
+                {
+                    MessageBox.Show("Invalid score value.");
+                    return;
+                }
+
+                if (rideId <= 0)
+                {
+                    MessageBox.Show("Invalid ride ID.");
+                    return;
+                }
+
+                MessageBox.Show($"About to update: Feedback = '{feedback}', Score = {score}, Ride ID = {rideId}");
+
+                string connectionString = ConfigurationManager.ConnectionStrings["DatabaseConnectionString"].ConnectionString;
+                string sqlQuery = @"UPDATE ride SET feedback = @Feedback, score = @Score WHERE id = @RideId";
+
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    con.Open();
+                    using (SqlCommand cmd = new SqlCommand(sqlQuery, con))
+                    {
+                        cmd.Parameters.AddWithValue("@Feedback", feedback);
+                        cmd.Parameters.AddWithValue("@Score", score);
+                        cmd.Parameters.AddWithValue("@RideId", rideId);
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        MessageBox.Show($"Rows affected: {rowsAffected}");
+                        if (rowsAffected > 0)
+                        {
+                            MessageBox.Show("Feedback and score saved successfully!");
+                            this.Close();
+                        }
+                        else
+                        {
+                            MessageBox.Show("No ride found with the given ID or update failed.");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error in FeedbackForm: {ex.Message}\nStack Trace: {ex.StackTrace}");
+            }
+
+        }
+    }
+}
